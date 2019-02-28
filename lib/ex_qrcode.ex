2583ad2fe7917f1a65a1a9d6505c66da80c0a96a
m31271n <m31271n@users.noreply.github.com>
2019-02-28 14:28:45 +0800

    init: initial commit

---
+++ lib/ex_qrcode.ex
@@ -0,0 +1,18 @@
+defmodule ExQrcode do
+  @moduledoc """
+  Documentation for ExQrcode.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExQrcode.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_qrcode_test.exs
@@ -0,0 +1,8 @@
+defmodule ExQrcodeTest do
+  use ExUnit.Case
+  doctest ExQrcode
+
+  test "greets the world" do
+    assert ExQrcode.hello() == :world
+  end
+end
